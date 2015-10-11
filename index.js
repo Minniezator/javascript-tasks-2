3ef4da7b5f84220cf4f416a15927d8fc85b42c21
Minniezator <minniezator17@yandex.ru>
2015-10-11 16:14:52 +0500

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
 var phoneBook = require('./phoneBook');
 
 // Добавляем записи
-phoneBook.add('Сергей', '7 999 6667778', 'gs@example.com');
-phoneBook.add('Сергей 2', '999 4433444', 'gs@example.com');
+phoneBook.add('Сергей', '7 999 666 7 778', 'gs@example.com');
+phoneBook.add('Сергей 2', '999 443 3 444', 'gs@example.com');
 phoneBook.add('Олег', '+7 (999) 777-7-777', 'just7@yandex-team.ru');
 
 // Невалидные данные не должны попадать в книгу!
@@ -31,5 +31,5 @@ phoneBook.showTable();
 
 
 // Экспортируем записи, пример файла рядом
-phoneBook.export('./backup.csv');
+//phoneBook.export('./backup.csv');
 // Добавлено 4 контакта
