1e49aaeda3990ece1aa338c407b440c691bf05b4
Minniezator <minniezator17@yandex.ru>
2015-10-20 01:00:32 +0500

    Update phone-book.js

--- phone-book.js
+++ phone-book.js
@@ -4,31 +4,50 @@ var phoneBook = [];
 var maxLengths = [];
 
 module.exports.add = function add(name, phone, email) {
+    if (checkExistence(name, phone, email)) {
+        console.log('Контакт уже есть в книге.');
+        return null;
+    }
     var phonePattern = /^((\+)?(\d{1,4}\s)?(\d{3}|(\(\d{3}\)))\s\d{3}((-\d{1}-)|(\s\d{1}\s))\d{3})|(\d{11,14})$/;
     var emailPattern = /^[a-zA-Z0-9_.+-]+@[a-zA-Zа-яА-Я0-9-]+(\.[a-zA-Zа-яА-Я0-9-.]+)+$/;
-    if (phonePattern.test(phone) && emailPattern.test(email) /*&& !module.exports.find(name) && !module.exports.find(email) && !module.exports.find(email)*/) {
+    if (phonePattern.test(phone) && emailPattern.test(email)) {
         phoneBook.push({name: name, phone: phone, email: email});
+    } else {
+        console.log('Данные введены неверно!');
     }
-    //else console.log('Данные введены неверно!');
     return null;
 };
 
+function checkExistence(name, phone, email) {
+    var isExist = false;
+    function tryContact(contact, index, book) {
+        if (contact.name === name ||
+            contact.phone === phone ||
+            contact.email === email) {
+            isExist = true;
+        }
+    }
+    phoneBook.forEach(tryContact);
+    return isExist;
+};
+
 module.exports.find = function find(query) {
+    query.replace(/[\+\s()-]/g, '');
     var isFound = false;
     function tryContact(contact, index, book) {
         function printContact(key, index, keys) {
             process.stdout.write(contact[key] + ', ');
         }
         function isInBook(key, index, keys) {
-            if ((new RegExp(query, 'i')).test(contact[key])) {
+            if ((new RegExp(query, 'i')).test(contact[key].replace(/[\+\s()-]/g, ''))) {
                 isFound = true;
                 keys.forEach(printContact);
+                console.log();
             }
         }
         Object.keys(contact).forEach(isInBook);
     }
     phoneBook.forEach(tryContact);
-
     return isFound;
 };
 
@@ -44,6 +63,7 @@ module.exports.remove = function remove(query) {
         }
         Object.keys(phoneBook[i]).forEach(removeContact);
     }
+    console.log('Удален ' + numOfRemoved + ' контакт');
     return numOfRemoved;
 };
 
@@ -66,7 +86,8 @@ function createMaxLengths() {
 }
 
 function drawHorizontal(lengthOfString) {
-    for (var i = 0; i < lengthOfString + 4 * Object.keys(phoneBook[0]).length; ++i) {
+    const CELL_PADDING = 4;
+    for (var i = 0; i < lengthOfString + CELL_PADDING * Object.keys(phoneBook[0]).length; ++i) {
         process.stdout.write('─');
     }
     console.log();
@@ -87,7 +108,7 @@ function drawHeader() {
 
 module.exports.showTable = function showTable() {
     if (phoneBook.length !== 0) {
-        var lengthOfString = createArrayOfMaxLengths();
+        var lengthOfString = createMaxLengths();
         drawHorizontal(lengthOfString);
         drawHeader();
         drawHorizontal(lengthOfString);
